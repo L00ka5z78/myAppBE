285fd9b32259f8bb58c39a9b6c55ef4e8848495a
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 09:56:50 +0100

    basic setup and db connection established

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectionDB } from "./database/connectionDB.js";
 
 const app = express();
 
 dotenv.config();
+
+connectionDB()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 app.listen(process.env.PORT, () => console.log(`Server is ON and running on PORT ${process.env.PORT}`))
---
+++ models/Todo.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+}, { timestamps: true });
+
+export const Todo = mongoose.model("Todo", TodoSchema);
