eb48b41ee4f67a6c6079ffbad0f630a30dcd6fb2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-26 09:23:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.listen(process.env.PORT, () => console.log(`Server is ON and running on PORT ${process.env.PORT}`))
