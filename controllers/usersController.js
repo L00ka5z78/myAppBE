8bb09a544156afaee36205e4e32e68b0e807cba2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-27 08:51:24 +0100

    register function in userController built

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,47 @@ import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt';
 import { Todo } from "../models/Todo.js";
 
-export const register = async (req, res) => { };
+export const register = async (req, res) => {
+    const { name, email, password, age } = req.body
+
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res
+                .status(400)
+                .json({ message: "User already exists..." })
+        }
+        // hash the password
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // create new user and insert in database
+
+        user = newUser({
+            name, email, age, password: hashedPassword,
+        })
+        await user.save();
+
+        const payload = {
+            user: user._id,
+        }
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 360000 })
+        res.cookie("token", token, {
+            httpOnly: true,
+            expiresIn: 360000
+        })
+        const { password: pass, ...rest } = user._doc;
+        res.status(201).json({ message: "User created successfully", user: rest })
+
+    } catch (error) {
+        console.error(error.message);
+        res
+            .status(500)
+            .json({ error: "Internal server error..." })
+    }
+};
 
 export const login = async (req, res) => { };
 
