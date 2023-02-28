08b8707b73f40384fd5c1dfa640a125c5bf4b967
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-02-28 12:11:10 +0100

    basic routes structure added

---
+++ controllers/todosController.js
@@ -0,0 +1,11 @@
+import { Todo } from "../models/Todo.js";
+
+export const getTodos = async(req, res) = + {}
+
+export const getTodo = async(req, res) = + {}
+
+export const createTodo = async(req, res) = + {}
+
+export const updateTodo = async(req, res) = + {}
+
+export const deleteTodo = async(req, res) = + {}
--- routes/todos.route.js
+++ routes/todos.route.js
@@ -1,4 +1,21 @@
 import express from 'express';
+import { authorize } from '../controllers/middleware/authorize.js'
+import { getTodo, getTodos, createTodo, updateTodo, deleteTodo } from '../controllers/todosController.js'
 
 const router = express.Router();
+
+// crud operations
+
+router.get("/:id", authorize, getTodo)
+
+router.get("/", authorize, getTodos)
+
+router.post("/create", authorize, createTodo)
+
+router.put("/update/:id", authorize, updateTodo)
+
+router.delete("/delete/:id", authorize, deleteTodo)
+
+
+
 export default router
