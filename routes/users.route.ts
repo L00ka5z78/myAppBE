32b79fff2aa005a168c13263d25f14e9611e97f9
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-03-08 14:25:37 +0100

    file extension changed, bunch of errors to fix

---
+++ routes/users.route.ts
@@ -0,0 +1,67 @@
+// import express from 'express';
+// import { register, login, logout, getMe, deleteUser, updateDetails, updatePassword } from '../controllers/usersController.ts';
+// import { authorize } from '../controllers/middleware/authorize.ts';
+// import { registerRequirements, loginRequirements, updateDetailsRequirements, updatePasswordRequirements } from '../controllers/middleware/validate.ts';
+// import { validateResult } from '../controllers/middleware/validationResults.ts';
+//
+// const router = express.Router();
+//
+// router.post('/register', registerRequirements, validateResult, register);
+// router.post("/login", loginRequirements, validateResult, login);
+//
+// router.get("/logout", authorize, logout);
+// router.get("/me", authorize, getMe);
+// router.put('/updatedetails', authorize, updateDetailsRequirements, validateResult, updateDetails);
+// router.put("/updatepassword", authorize, updatePasswordRequirements, validateResult, updatePassword);
+//
+// // router.delete("/delete", authorize, deleteUser);
+// router.delete("/delete", deleteUser);
+//
+//
+// export default router
+
+import * as express from 'express';
+import {
+    register,
+    login,
+    logout,
+    getMe,
+    deleteUser,
+    updateDetails,
+    updatePassword,
+} from '../controllers/usersController';
+import { authorize } from '../controllers/middleware/authorize';
+import {
+    registerRequirements,
+    loginRequirements,
+    updateDetailsRequirements,
+    updatePasswordRequirements,
+} from '../controllers/middleware/validate';
+import { validateResult } from '../controllers/middleware/validationResults';
+
+const router = express.Router();
+
+router.post('/register', registerRequirements, validateResult, register);
+router.post('/login', loginRequirements, validateResult, login);
+router.get('/logout', authorize, logout);
+
+router.get('/me', authorize, getMe);
+router.put(
+    '/updatedetails',
+    authorize,
+    updateDetailsRequirements,
+    validateResult,
+    updateDetails
+);
+router.put(
+    '/updatepassword',
+    authorize,
+    updatePasswordRequirements,
+    validateResult,
+    updatePassword
+);
+
+router.delete('/delete', authorize, deleteUser);
+// router.delete("/delete", deleteUser);    //bug with delete
+
+export default router;
